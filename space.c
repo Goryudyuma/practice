af7d7cbdc37f8e51ab6471b5be5460149de985e9
Goryudyuma <umaretekyoumade@gmail.com>
2014-12-15 12:28:01 +0900

    スペースでいじいじ
    これきちんとアップロードできんのかな・・・
    unicodeで実行お願いします。

---
+++ space.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+typedef int ​​;
+#define ​​​​ return 0;
+#define ​​​​​ "%d"
+#define ​​​​​​ "%d\n"
+#define ​​​​​​​ scanf ( ​​​​​, &​ );
+#define ​​​​​​​​ printf ( ​​​​​​, ​*​ ); ​​​​
+#define ​​​​​​​​​​ main ( ​ )
+​​  ​​​​​​​​​​{
+	​​​​​​​
+		​​​​​​​​
+}
